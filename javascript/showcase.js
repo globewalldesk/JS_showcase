fd63edaf3621f5964b4bf99f793a3b329a4883f4
globewalldesk <yo.larrysanger@gmail.com>
2017-09-09 22:00:37 -0400

    fixing issue with Windows & Linux support

--- javascript/showcase.js
+++ javascript/showcase.js
@@ -13,12 +13,7 @@ function load_present_file_for_code_reporter() {
   xhr.onreadystatechange = function(){
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        // What line ending to split on depends on the OS.
-        if (xhr.responseText.match(/\\r\\n/)) {
-          this_file = xhr.responseText.split("\r\n");
-        } else {
-          this_file = xhr.responseText.split("\n");
-        }
+        this_file = xhr.responseText.split("\r\n");
       } else {
         present_file_loadable = false;
       }
